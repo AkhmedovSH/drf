0afd5c455469bc14d0648b7cedc817e4fec0aed1
Answers <shurikaxmedov1@gmail.com>
2021-01-24 23:48:48 +0500

    done

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from .views import article_list, article_detail, ArticleApiView, ArticleDetailApiView
+from django.urls.conf import include
+from .views import *
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register('article', ArticleViewSet, basename='article')
 
 urlpatterns = [
+	path('viewset/', include(router.urls)),
 	#path('article/', article_list),
 	path('article/', ArticleApiView.as_view()),
 	#path('detail/<int:pk>/', article_detail),
 	path('detail/<int:id>/', ArticleDetailApiView.as_view()),
+	path('generic/article/', GenericApiView.as_view()),
+	path('generic/article/<int:id>', GenericApiView.as_view()),
 ]
--- api_basic/views.py
+++ api_basic/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from rest_framework import serializers
+from rest_framework import request, serializers
 from rest_framework.parsers import JSONParser
 from .models import Article
 from .serializers import ArticleSerializer
@@ -9,8 +9,45 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework import mixins
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
+class ArticleViewSet(viewsets.ViewSet):
+	
+
+class GenericApiView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin,
+ mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
+	serializer_class = ArticleSerializer
+	queryset = Article.objects.all()
+
+	lookup_field = 'id'
+	#authentication_classes = [SessionAuthentication, BasicAuthentication]
+	authentication_classes = [TokenAuthentication]
+	permission_classes = [IsAuthenticated]
+
+	def get(self, request, id=None):
+		if id:
+			return self.retrieve(request)
+		else:
+			return self.list(request)
+
+	def post(self, request):
+		return self.create(request)
+
+	def put(self, request, id=None):
+		return self.update(request, id)
+
+	def delete(self, request, id):
+		return self.destroy(request, id)
+			
+
+
 
 class ArticleApiView(APIView):
 	def get(self, request):
